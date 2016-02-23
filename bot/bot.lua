6616d7f93aafd72a35cb99e2b805c12f79b983d7
sina7skk <hacker.7sk.33@gmail.com>
2016-02-23 15:57:51 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {137791771},
+    sudo_users = {114934518,137791771},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
